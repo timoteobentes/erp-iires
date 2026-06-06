3920ae9d80d0201ef7ebc527c24605379ba5fee9
Timóteo Bentes <bentestimoteo@gmail.com>
2026-06-05 20:35:35 -0400

    feat: ajuste hosts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -28,7 +28,7 @@ const corsOptions: cors.CorsOptions = {
 };
 
 // Preflight OPTIONS deve ser respondido antes de qualquer outro middleware
-app.options('*', cors(corsOptions));
+app.options('/(.*)', cors(corsOptions));
 app.use(cors(corsOptions));
 
 app.use(helmet());
